306cfcc81392a1209d9f4ab987b1ed0f6f823767
mosabkullab <mosab.kullab@gmail.com>
2021-04-14 21:15:19 +0300

    third commit

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,21 @@ Route::post('send', function(HttpRequest $request){
         $name = $request ->myname ;
     return view('about',compact('name'));
 });
+Route::get('tasks', function () {
+    $tasks = [
+       'first-task' =>'task1',
+       'second-task' => 'task2',
+       'third-task' => 'task3'
+    ];
+    return view('tasks',compact('tasks'));
+});
+route::get('show/{id}', function ($id) {
+    $tasks = [
+        'first-task' =>'task1',
+        'second-task' => 'task2',
+        'third-task' => 'task3'
+     ];
+    $task = $tasks [$id] ;
+return view('show',compact('task'));
+
+});
